16810738a844c7c22adaf8934e4ea59ba6344eaf
Srimukh Seeram <srimukhseeram@Srimukhs-MacBook-Air.local>
2026-03-11 12:11:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import { BrowserRouter } from "react-router-dom"
-import { Route } from "react-router-dom"
-import { Routes } from "react-router-dom"
-import Home from "./Home"
-import Reg from "./Register"
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Reg from "./Reg";
+import Home from "./Home";
 
-function App()
-{
-  return(
+function App() {
+  return (
     <>
-
-      <h1>I am App</h1>
+      <h1>This is App</h1>
       <BrowserRouter>
-      <Routes>
-        <Route path = "/reg" element = {<Reg/>}></Route>
-        <Route path = "/h" element = {<Home/>}></Route>
-      </Routes>
+        <Routes>
+          <Route path="/reg" element={<Reg />} />
+          <Route path="/h" element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
