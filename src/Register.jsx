2e9faa73345be0ce3be752adab022f28bd71bc32
srimukh01 <23eg104d61@anurag.edu.in>
2026-03-11 13:32:01 +0530

    Update Register.jsx

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,43 +1,55 @@
 import axios from "axios";
 import { useState } from "react";
 
-function Reg() {
-  const [data, setdata] = useState({
+function Register() {
+  const [data, setData] = useState({
     username: "",
     email: "",
     password: "",
   });
+
   const changeName = (e) => {
-    setdata({ ...data, [e.target.name]: e.target.value });
+    setData({ ...data, [e.target.name]: e.target.value });
   };
+
   const submit = async () => {
     try {
-      const res = await axios.post("https://cab-f9u5.onrender.com", data);
+      const res = await axios.post(
+        "https://cab-f9u5.onrender.com/register",
+        data
+      );
       alert(res.data);
-    } catch (xyz) {
-      alert(xyz.response.data);
+    } catch (err) {
+      alert(err.response?.data || "Error occurred");
     }
   };
+
   return (
     <>
-      <h1>This is App</h1>
+      <h1>Register</h1>
+
       <input
         onChange={changeName}
         name="username"
-        placeholder="Enter the username"
-      ></input>
+        placeholder="Enter username"
+      />
+
       <input
         onChange={changeName}
         name="email"
-        placeholder="Enter the email"
-      ></input>
+        placeholder="Enter email"
+      />
+
       <input
+        type="password"
         onChange={changeName}
         name="password"
-        placeholder="create password"
-      ></input>
-      <button onClick={submit}>register</button>
+        placeholder="Create password"
+      />
+
+      <button onClick={submit}>Register</button>
     </>
   );
 }
-export default Reg;
+
+export default Register;
