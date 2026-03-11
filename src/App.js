d89536703bcc18ab1ec3f82990ce9ec3d2931cc3
srimukh01 <23eg104d61@anurag.edu.in>
2026-03-11 12:27:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
-import Reg from "./Reg";
+import Reg from "./Register";
 import Home from "./Home";
 
 function App() {
